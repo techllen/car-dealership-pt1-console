869a6d981278a74c77c0aad0d267fe2479c578f3
THE-Allen <allenmwita21@gmail.com>
2021-06-25 11:56:39 -0400

    user functions implementation

--- src/main/java/com/techllenapps/cardealershipconsoleapp/functions/Driver.java
+++ src/main/java/com/techllenapps/cardealershipconsoleapp/functions/Driver.java
@@ -1,11 +1,12 @@
 package com.techllenapps.cardealershipconsoleapp.functions;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Driver{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		UserFunctions usroperation = new UserFunctions();
 		boolean stickMenu = true;
 		//receiving keyboard input
--- src/main/java/com/techllenapps/cardealershipconsoleapp/functions/UserFunctions.java
+++ src/main/java/com/techllenapps/cardealershipconsoleapp/functions/UserFunctions.java
@@ -1,5 +1,6 @@
 package com.techllenapps.cardealershipconsoleapp.functions;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -14,51 +15,51 @@ import com.techllenapps.cardealershipconsoleapp.entities.User;
 
 public class UserFunctions extends User{
 
-	/**
-	 * serial id
-	 */
 	private static final long serialVersionUID = 1L;
 	Scanner scan = new Scanner(System.in);
 	User user = new User();
-	ArrayList<HashMap<String, String>> allUsers = new ArrayList<HashMap<String, String>>();
+	HashMap<String, String> allUsersMap = new HashMap<String, String>();
+	ArrayList<HashMap<String, String>> allUsersList = new ArrayList<HashMap<String,String>>();
+	FileOutputStream fout;
+	FileInputStream fin;
 
+	public void login() throws IOException {
 
-
-	public void login() {
-		
-//		System.out.println("*****************************************************\n");
-//		System.out.println("Please enter your username and password to login");
-//		System.out.println("\n****************************************************");
-//		System.out.println("Username:");
-//		String enteredUserName = scan.nextLine();
-//		user.setUserName(enteredUserName);
-//		System.out.println("Password:");
-//		String enteredPassCode = scan.nextLine();
-//		user.setPassWord(enteredPassCode);
-//		//assign passcode and username to the hashmap as key value respectively
-//		users.put(user.getPassWord(), user.getUserName());
-//		//serialising the user credentials for persistence
-		FileInputStream fin;
+		//		System.out.println("*****************************************************\n");
+		//		System.out.println("Please enter your username and password to login");
+		//		System.out.println("\n****************************************************");
+		//		System.out.println("Username:");
+		//		String enteredUserName = scan.nextLine();
+		//		user.setUserName(enteredUserName);
+		//		System.out.println("Password:");
+		//		String enteredPassCode = scan.nextLine();
+		//		user.setPassWord(enteredPassCode);
+		//		//assign passcode and username to the hashmap as key value respectively
+		//		users.put(user.getPassWord(), user.getUserName());
+		//		//serialising the user credentials for persistence
 		ArrayList<HashMap<String, String>> retrievedUsers = new ArrayList<HashMap<String, String>>();
+		fin = new FileInputStream("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser");
+		ObjectInputStream in = new ObjectInputStream(fin);
+		while(fin.available() > 0) {
 		try {
-			fin = new FileInputStream("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser");
-			ObjectInputStream in = new ObjectInputStream(fin);
 			retrievedUsers = (ArrayList<HashMap<String, String>>)in.readObject();
-			in.close();
-			fin.close();
-		} catch (FileNotFoundException e){
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//in.close();
+			//fin.close();
+//		} catch (FileNotFoundException e){
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println(retrievedUsers);
+		System.out.println(retrievedUsers.size());
 		//boolean available = false;
 		//return available;
+	}
 	}
 
 	public  void register() {
@@ -67,24 +68,31 @@ public class UserFunctions extends User{
 		System.out.println("\n****************************************************");
 		System.out.println("Username:");
 		String registeredUserName = scan.nextLine();
-		User usr = new User();
-		usr.setUserName(registeredUserName);
+		user.setUserName(registeredUserName);
 		System.out.println("Password:");
 		String passcode = scan.nextLine();
-		usr.setPassWord(passcode);
+		user.setPassWord(passcode);
 		//assign passcode and username to the hashmap as key value respectively
-		HashMap<String,String> users = new HashMap<String,String>();
-		users.put(usr.getPassWord(), usr.getUserName());
+		allUsersMap.put(user.getPassWord(), user.getUserName());
+		//adding the users hashmap to the arraylist
+		allUsersList.add(allUsersMap);
 		//serialising the user credentials for persistence
-		FileOutputStream fout;
 		try {
-			fout = new FileOutputStream("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser");
-			ObjectOutputStream out = new ObjectOutputStream(fout);
-			//HashMap<String,String> usersToAdd = new HashMap<String,String>();
-			allUsers.add(users);
-			out.writeObject(allUsers);
-			out.close();
-			fout.close();
+			boolean exists = new File("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser\"").exists();
+			fout = new FileOutputStream("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser",true);
+			//ObjectOutputStream out = new ObjectOutputStream(fout);
+			ObjectOutputStream out = exists ? 
+					new ObjectOutputStream(fout) {
+
+				protected void writeStreamHeader() throws IOException {
+					reset();
+				}
+			}:new ObjectOutputStream(fout);
+
+			out.writeObject(allUsersList);
+			System.out.println("\n SAVED!!!!");
+			//out.close();
+			//fout.close();
 		} catch (FileNotFoundException e){
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -92,4 +100,5 @@ public class UserFunctions extends User{
 			e.printStackTrace();
 		}
 	}
+
 }
