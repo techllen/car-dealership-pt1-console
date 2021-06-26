b4c7e90a994dbc9419f9e56603fb343b319d56cf
THE-Allen <allenmwita21@gmail.com>
2021-06-26 06:46:24 -0400

    user functions implementation

--- src/main/java/com/techllenapps/cardealershipconsoleapp/functions/UserFunctions.java
+++ src/main/java/com/techllenapps/cardealershipconsoleapp/functions/UserFunctions.java
@@ -13,13 +13,15 @@ import java.util.Scanner;
 
 import com.techllenapps.cardealershipconsoleapp.entities.User;
 
+import jdk.internal.org.jline.reader.EndOfFileException;
+
 public class UserFunctions extends User{
 
 	private static final long serialVersionUID = 1L;
 	Scanner scan = new Scanner(System.in);
 	User user = new User();
 	HashMap<String, String> allUsersMap = new HashMap<String, String>();
-	ArrayList<HashMap<String, String>> allUsersList = new ArrayList<HashMap<String,String>>();
+	//ArrayList<HashMap<String, String>> allUsersList = new ArrayList<HashMap<String,String>>();
 	FileOutputStream fout;
 	FileInputStream fin;
 
@@ -37,19 +39,22 @@ public class UserFunctions extends User{
 		//		//assign passcode and username to the hashmap as key value respectively
 		//		users.put(user.getPassWord(), user.getUserName());
 		//		//serialising the user credentials for persistence
-		ArrayList<HashMap<String, String>> retrievedUsers = new ArrayList<HashMap<String, String>>();
+		//ArrayList<HashMap<String, String>> retrievedUsers = new ArrayList<HashMap<String, String>>();
+		HashMap<String, String> retrievedUsers = new HashMap<String, String>();
 		fin = new FileInputStream("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser");
 		ObjectInputStream in = new ObjectInputStream(fin);
-		while(fin.available() > 0) {
 		try {
-			retrievedUsers = (ArrayList<HashMap<String, String>>)in.readObject();
+			//retrievedUsers = (ArrayList<HashMap<String, String>>)in.readObject();
+			Object obj=null;
+			while ((boolean) (obj=in.readObject() instanceof Endoffile ==false)) {
+			retrievedUsers = (HashMap<String, String>)in.readObject();
 			//in.close();
 			//fin.close();
-//		} catch (FileNotFoundException e){
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
+			//		} catch (FileNotFoundException e){
+			//			e.printStackTrace();
+			//		} catch (IOException e) {
+			//			// TODO Auto-generated catch block
+			//			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -59,7 +64,7 @@ public class UserFunctions extends User{
 		System.out.println(retrievedUsers.size());
 		//boolean available = false;
 		//return available;
-	}
+
 	}
 
 	public  void register() {
@@ -75,30 +80,19 @@ public class UserFunctions extends User{
 		//assign passcode and username to the hashmap as key value respectively
 		allUsersMap.put(user.getPassWord(), user.getUserName());
 		//adding the users hashmap to the arraylist
-		allUsersList.add(allUsersMap);
+		//allUsersList.add(allUsersMap);
 		//serialising the user credentials for persistence
 		try {
-			boolean exists = new File("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser\"").exists();
 			fout = new FileOutputStream("//media//techllen//01D5CEDF6FF7FE50//Development//5.PROJECTS//car-dealership-pt1-console//src//main//resources//users.ser",true);
-			//ObjectOutputStream out = new ObjectOutputStream(fout);
-			ObjectOutputStream out = exists ? 
-					new ObjectOutputStream(fout) {
-
-				protected void writeStreamHeader() throws IOException {
-					reset();
-				}
-			}:new ObjectOutputStream(fout);
-
-			out.writeObject(allUsersList);
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			out.writeObject(allUsersMap);
 			System.out.println("\n SAVED!!!!");
 			//out.close();
 			//fout.close();
 		} catch (FileNotFoundException e){
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
 }
