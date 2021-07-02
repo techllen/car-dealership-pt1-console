e4ae511d0922180b2fcd40191b948db5bb90ff8e
THE-Allen <allenmwita21@gmail.com>
2021-07-01 20:35:25 -0400

    Menu implementation

--- src/main/java/com/techllenapps/cardealershipconsoleapp/functions/Driver.java
+++ src/main/java/com/techllenapps/cardealershipconsoleapp/functions/Driver.java
@@ -11,36 +11,65 @@ public class Driver{
 		boolean stickMenu = true;
 		//receiving keyboard input
 		Scanner scan = new Scanner(System.in);
-
-		do {
-
+		
+		while(true) {
 			System.out.println("****************************************************************\n");
 			System.out.println("Wellcome to the Car Dealesrhip Console Application Ver 1.01\n");
+			System.out.println("This is a simple console based Java App for learning purposes");
 			System.out.println("****************************************************************");
-			System.out.println("Please tell us what you want to do today select 1 to Signin and 2 to Register and Account\n");
-			System.out.println("1.Register an account\n");
-			System.out.println("2.Sign in\n");
-			try {
-				int menuSelection = scan.nextInt();
+			System.out.println("Please select 1 to log in as Admin or 2 to log in as Customer/Employee");
+			//the administrator will assign employees and their roles
+			System.out.println("1.Administrator");
+			System.out.println("2.User(Customer/Employee)\n");
+			int typeOfEmployeeChoice = scan.nextInt();
+			
+			switch (typeOfEmployeeChoice) {
+			case 1:
+				//implement Admini menu
+				//adminiMenu();
+				//checkSystemLogs();
+				break;
+				
+			case 2:
+				//First check the user type customer/employee
+				//checkUserType();Return employee type
+				
+				//Open the menu according to which type of the user has logged in
+				//openUserMenu();
+				
+				break;
 
-				//calling sign in or register function if appropriate selection is selected 
-				//break the loop right after the condition is fulfilled
-				if (menuSelection==1) {
-					usroperation.register();
-					break;
-				}else if (menuSelection==2) {
-					usroperation.login();
-					break;
-				}else {
-					System.out.println("\nplease select 1 or 2");
-					break;
-				}
-			}catch (InputMismatchException i) {
-				System.out.println("\nplease enter integer 1 or 2");
+			default:
+				System.out.println("Invalid Choice,Please select 1 or 2");
 				break;
 			}
+
+
+			
+//			System.out.println("Please tell us what you want to do today select 1 to Signin and 2 to Register and Account\n");
+//			System.out.println("1.Register an account\n");
+//			System.out.println("2.Sign in\n");
+//			try {
+//				int menuSelection = scan.nextInt();
+//
+//				//calling sign in or register function if appropriate selection is selected 
+//				//break the loop right after the condition is fulfilled
+//				if (menuSelection==1) {
+//					usroperation.register();
+//					break;
+//				}else if (menuSelection==2) {
+//					usroperation.login();
+//					break;
+//				}else {
+//					System.out.println("\nplease select 1 or 2");
+//					break;
+//				}
+//			}catch (InputMismatchException i) {
+//				System.out.println("\nplease enter integer 1 or 2");
+//				break;
+//			}
 		}
-		while (stickMenu==true);
+		
 		scan.close();
 	}
 }
